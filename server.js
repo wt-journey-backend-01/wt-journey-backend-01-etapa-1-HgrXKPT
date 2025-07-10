3e2b4e79a4b6fb9b58c1fc01a5cc406ef24030e6
HgrXKPT <higorestevao02@hotmail.com>
2025-07-10 20:43:14 -0300

    Resolvendo conflitos

--- server.js
+++ server.js
@@ -2,18 +2,35 @@ const express = require('express')
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const fs = require('fs');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); // Para formulários HTML
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
+    try{
+        res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
+    }catch (error) {
+        errorProcessarSugestao(error,res);
+    }
+    
 });
 
 app.get('/sugestao', (req, res) => {
     const nome  = req.query.Nome;
     const ingredientes = req.query.Ingredientes;
-    res.status(200).send(`Obrigado, ${nome}! Seus ingredientes "${ingredientes}" foi recebida.`);;
+
+    if (!nome || !ingredientes) {
+        return res.status(400).send('Nome e Ingredientes são obrigatórios.');
+    };
+    try{
+         res.status(200).send(`Obrigado, ${nome}! Seus ingredientes "${ingredientes}" foi recebida.`);;
+    }catch (error) {
+        errorProcessarSugestao(error,res);
+    }
+
+    
+   
 });
 
 
@@ -33,8 +50,22 @@ app.post('/contato', (req, res) => {
 });
 
 app.get('/api/lanches', (req, res) => {
+    try{
+        const data = fs.readFileSync(path.join(__dirname,'public' , 'data', 'lanches.json'), 'utf8');
+        const lanches = JSON.parse(data);
+        res.status(200).json(lanches);
+    }catch (error) {
+        errorProcessarSugestao(error,res);
+    }
+
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor da DevBurger rodando em http://localhost:${PORT}`);
 });
+
+
+function errorProcessarSugestao(error,res) {
+    console.error('Erro ao processar a sugestão:', error);
+    res.status(500).send('Erro ao processar a sugestão.');
+}
